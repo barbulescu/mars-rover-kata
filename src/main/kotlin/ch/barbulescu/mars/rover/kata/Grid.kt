289d6f2d9b6301174020111f47b0850bc6492b92
Marius Barbulescu <marius.barbulescu@gmail.com>
2024-11-22 21:37:27 +0100

    move rover on grid

--- src/main/kotlin/ch/barbulescu/mars/rover/kata/Grid.kt
+++ src/main/kotlin/ch/barbulescu/mars/rover/kata/Grid.kt
@@ -1,3 +1,7 @@
 package ch.barbulescu.mars.rover.kata
 
-class Grid(val rows: Int, val cols: Int)
+class Grid(val rows: Int, val cols: Int) {
+    fun move(rover: Rover, orientation: Orientation): Rover {
+        return rover.move(orientation)
+    }
+}
--- src/test/kotlin/ch/barbulescu/mars/rover/kata/GridTest.kt
+++ src/test/kotlin/ch/barbulescu/mars/rover/kata/GridTest.kt
@@ -1,9 +1,44 @@
 package ch.barbulescu.mars.rover.kata
 
-import org.assertj.core.api.Assertions
-import org.assertj.core.api.Assertions.*
+import ch.barbulescu.mars.rover.kata.Orientation.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class GridTest {
 
+    @Test
+    fun `move rover north`() {
+        val grid = Grid(5, 5)
+        val rover = Rover(1, 1)
+        val movedRover = grid.move(rover, NORTH)
+
+        assertThat(movedRover).isEqualTo(Rover(1, 0))
+    }
+
+    @Test
+    fun `move rover east`() {
+        val grid = Grid(5, 5)
+        val rover = Rover(1, 1)
+        val movedRover = grid.move(rover, EAST)
+
+        assertThat(movedRover).isEqualTo(Rover(2, 1))
+    }
+
+    @Test
+    fun `move rover south`() {
+        val grid = Grid(5, 5)
+        val rover = Rover(1, 1)
+        val movedRover = grid.move(rover, SOUTH)
+
+        assertThat(movedRover).isEqualTo(Rover(1, 2))
+    }
+
+    @Test
+    fun `move rover west`() {
+        val grid = Grid(5, 5)
+        val rover = Rover(1, 1)
+        val movedRover = grid.move(rover, WEST)
+
+        assertThat(movedRover).isEqualTo(Rover(0, 1))
+    }
 }
